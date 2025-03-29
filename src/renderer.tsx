ed8651b517422af6a2edafd2c3dc7ea284c893b7
kobaken <kentafly88@gmail.com>
2025-03-29 12:22:10 +0900

    Added tailwindcss

--- src/renderer.tsx
+++ src/renderer.tsx
@@ -4,7 +4,11 @@ export const renderer = reactRenderer(({ children }) => {
   return (
     <html>
       <head>
-        <link href="/src/index.css" rel="stylesheet" />
+        { !!import.meta.env ? (
+          <link href="/src/index.css" rel="stylesheet" />
+        ) : (
+          <link href="/static/style.css" rel="stylesheet" />
+        ) }
       </head>
       <body>{children}</body>
     </html>
--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,31 @@ import { defineConfig } from 'vite'
 import ssrHotReload from 'vite-plugin-ssr-hot-reload'
 import tailwindcss from '@tailwindcss/vite'
 
-export default defineConfig({
-  ssr: {
-    external: ['react', 'react-dom'],
-  },
-  plugins: [ssrHotReload(), cloudflare(), tailwindcss()],
+export default defineConfig(({ mode }) => {
+  if (mode === 'client') {
+    return {
+      plugins: [ tailwindcss() ],
+      build: {
+        rollupOptions: {
+          input: {
+            style: "./src/index.css",
+          },
+          output: {
+            dir: "./dist",
+            assetFileNames: "static/[name].[ext]",
+          },
+        },
+        copyPublicDir: false,
+        minify: true,
+      }
+    }
+  }
+  else {
+    return {
+      ssr: {
+        external: ['react', 'react-dom'],
+      },
+      plugins: [ssrHotReload(), cloudflare(), tailwindcss()],
+    }
+  }
 })
