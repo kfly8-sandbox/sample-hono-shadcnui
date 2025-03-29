13f7cff199b1a72f5f4f347b517420e8b785d920
kobaken <kentafly88@gmail.com>
2025-03-29 12:43:48 +0900

    Added @/ alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,21 @@
+import path from "path"
 import { cloudflare } from '@cloudflare/vite-plugin'
 import { defineConfig } from 'vite'
 import ssrHotReload from 'vite-plugin-ssr-hot-reload'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig(({ mode }) => {
+  const commonConfig = {
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    }
+  }
+
   if (mode === 'client') {
     return {
+      ...commonConfig,
       plugins: [ tailwindcss() ],
       build: {
         rollupOptions: {
@@ -24,6 +34,7 @@ export default defineConfig(({ mode }) => {
   }
   else {
     return {
+      ...commonConfig,
       ssr: {
         external: ['react', 'react-dom'],
       },
