33ad8d0a1713ec8e02a38c8e69210e63c65b1146
kobaken <kentafly88@gmail.com>
2025-03-29 12:48:04 +0900

    Added Button

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,18 @@
 import { Hono } from 'hono'
 import { renderer } from './renderer'
+import { Button } from "@/components/ui/button"
 
 const app = new Hono()
 
 app.use(renderer)
 
 app.get('/', (c) => {
-  return c.render(<h1 className="text-3xl font-bold underline">Hello!</h1>)
+  return c.render(
+    <>
+      <h1 className="text-3xl font-bold underline">Hello!</h1>
+      <Button variant="secondary">Click me</Button>
+    </>
+  )
 })
 
 export default app
