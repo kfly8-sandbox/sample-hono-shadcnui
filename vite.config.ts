658b33e1128d2d14648fd0a967233cae8d02473e
kobaken <kentafly88@gmail.com>
2025-03-29 16:16:17 +0900

    Copy public dir

--- vite.config.ts
+++ vite.config.ts
@@ -27,7 +27,7 @@ export default defineConfig(({ mode }) => {
             assetFileNames: "static/[name].[ext]",
           },
         },
-        copyPublicDir: false,
+        copyPublicDir: true,
         minify: true,
       }
     }
